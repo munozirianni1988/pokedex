1e92d8d8a362e89e2467ad6f2fb7b99412b86053
Irianni Munoz <82213740+munozirianni1988@users.noreply.github.com>
2023-06-10 07:15:09 +0100

    clickable image

--- src/Logo.js
+++ src/Logo.js
@@ -1,11 +1,18 @@
 import React from "react";
 
+function logWhenClicked() {
+  console.log("React is fun!");
+}
+
 const Logo = (props) => {
   return (
     <header>
       <div>
         <h1>Welcome to {props.appName}</h1>
-        <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"></img>
+        <img
+          src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
+          onClick={logWhenClicked}
+        ></img>
       </div>
     </header>
   );
